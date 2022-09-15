821cf505d7c7f68bad9089d06949e9ef0495321d
JohnArdiente <officialjla@gmail.com>
2022-09-15 16:29:59 +0800

    Add project files.

---
+++ TatakPinoy/Controllers/ShipmentsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TatakPinoy.Data;
+using TatakPinoy.Models;
+
+namespace TatakPinoy.Controllers
+{
+    public class ShipmentsController : Controller
+    {
+        private readonly TatakPinoyContext _context;
+
+        public ShipmentsController(TatakPinoyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shipments
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Shipment.ToListAsync());
+        }
+
+        // GET: Shipments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shipment = await _context.Shipment
+                .FirstOrDefaultAsync(m => m.ShipmentId == id);
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(shipment);
+        }
+
+        // GET: Shipments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shipments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ShipmentId,Name,Address,ContactNo")] Shipment shipment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shipment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shipment);
+        }
+
+        // GET: Shipments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shipment = await _context.Shipment.FindAsync(id);
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+            return View(shipment);
+        }
+
+        // POST: Shipments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ShipmentId,Name,Address,ContactNo")] Shipment shipment)
+        {
+            if (id != shipment.ShipmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shipment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShipmentExists(shipment.ShipmentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shipment);
+        }
+
+        // GET: Shipments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shipment = await _context.Shipment
+                .FirstOrDefaultAsync(m => m.ShipmentId == id);
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(shipment);
+        }
+
+        // POST: Shipments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shipment = await _context.Shipment.FindAsync(id);
+            _context.Shipment.Remove(shipment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShipmentExists(int id)
+        {
+            return _context.Shipment.Any(e => e.ShipmentId == id);
+        }
+    }
+}
---
+++ TatakPinoy/Data/TatakPinoyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TatakPinoy.Models;
+
+namespace TatakPinoy.Data
+{
+    public class TatakPinoyContext : DbContext
+    {
+        public TatakPinoyContext (DbContextOptions<TatakPinoyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TatakPinoy.Models.Shipment> Shipment { get; set; }
+    }
+}
